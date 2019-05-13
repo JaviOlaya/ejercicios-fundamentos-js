fd80ba9a2189b8680a5ac6742f9bce6783ce71d2
Jose Javier Olaya Sáez <jjosaez@gmail.com>
2019-05-13 12:13:42 +0200

    ej 2-bucle-for

--- 1-bucles/2-bucle-for.js
+++ 1-bucles/2-bucle-for.js
@@ -1,3 +1,8 @@
 // ej 2
 // haz un bucle que muestre por consola los impares del 3 al 17 inclusive
 // Utilizad el bucle for
+var n=0;
+for (var i = 1; n <=17 ; i++) {
+    n=(2*i)+1;
+    console.log(n);
+}
